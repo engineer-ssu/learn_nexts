6ffc1248bbbc5e3ccaf5203a0a61142b079fdd8c
suyeon <developer.ssu.ssu@gmail.com>
2024-09-22 21:18:41 +0900

    feat : metadata 테스트

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+  
+export const metadata: Metadata = {
+  title: '노노노쑤쑤쑤',
+  description: '노노노쑤쑤쑤의 learn nextJS!',
+}
  
 export default function RootLayout({
   children,
